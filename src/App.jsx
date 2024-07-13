be88289efe912cb975ef3ed2551311bc8a4d4247
joab <eyoabamare3@gmial.com>
2024-07-13 21:30:57 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { Route, RouterProvider, createRoutesFromElements } from 'react-router';
-import { createBrowserRouter } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import HomePage from './pages/HomePage';
 
-
 const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-      </Route>
-    )
+  return (
+    <HashRouter basename={'/react-new-features/'}>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
-  <BrowserRouter basename={'/react-new-features/'}></BrowserRouter>
-  
-  return <RouterProvider router={router} />;
 };
 
 export default App;
